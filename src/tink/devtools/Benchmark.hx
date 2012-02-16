f2d9d501aecf419e9f77c7693e1701ef2fcb58ec
back2dos <back2dos@googlemail.com>
2012-02-16 12:49:58 +0100

    Created separate devtools package.

---
+++ src/tink/devtools/Benchmark.hx
@@ -0,0 +1,49 @@
+package tink.devtools;
+
+/**
+ * ...
+ * @author back2dos
+ */
+#if macro
+	import haxe.macro.Expr;
+	import tink.macro.tools.AST;
+	import tink.macro.tools.ExprTools;
+	using tink.macro.tools.ExprTools;
+	using tink.core.types.Outcome;
+#end
+class Benchmark {
+	var msg:String;
+	var last:Float;
+	public function new(?msg) {
+		this.next(msg);
+	}
+	inline function stamp() {
+		return
+			#if neko
+				neko.Sys.cpuTime();
+			#else
+				haxe.Timer.stamp();
+			#end
+	}
+	public function next(?msg:String) {
+		var now = stamp();
+		if (this.msg != null) {
+			trace(this.msg + ' took ' + (now - last));
+		}
+		this.last = now;
+		this.msg = msg;
+	}
+	public function finish<T>(?value:T) {
+		next();
+		return value;
+	}
+	@:macro static public function measure(msg:String, e:Expr, ?times:Expr):Expr {
+		if (times.getIdent().equals('null')) times = 1.toExpr();
+		return AST.build({
+			var tmpB = new Benchmark($(msg.toExpr()));
+			for (tmp in 0...$times - 1) $e;
+			tmpB.finish($e);
+		});		
+	}
+
+}
