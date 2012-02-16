c698afbe05043f257337175edc92adfcd6963a30
back2dos <back2dos@googlemail.com>
2012-02-16 23:58:25 +0100

    Added object hashing method.

--- src/tink/native/PHP.hx
+++ src/tink/native/PHP.hx
@@ -12,4 +12,13 @@ class PHP {
 	@:macro static public function embed(s:String) {
 		return AST.build(untyped __php__('eval__s'));
 	}
+	static public function objHash(key:Dynamic):String {
+		if (embed('is_array($key)')) {
+			if (embed('is_callable($key)')) 
+				return objHash(key[0]) + key[1];
+			else 
+				throw 'cannot handle native PHP arrays yet';
+		}
+		else return embed('spl_object_hash($key)');
+	}
 }
