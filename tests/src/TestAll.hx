05bb6b903a46f2b049178a848c6058a8425dc185
back2dos <back2dos@googlemail.com>
2012-03-08 12:00:12 +0100

    Added bindings to main test runner.

--- tests/src/TestAll.hx
+++ tests/src/TestAll.hx
@@ -9,12 +9,13 @@ import haxe.unit.TestRunner;
 class TestAll {
 	static public function run() {
 		var runner = new TestRunner();
+		runner.add(new util.PropertyTest());
 		runner.add(new lang.ClsTest());
 		runner.add(new lang.ExtTest());
-		runner.add(new util.PropertyTest());
+		runner.add(new collections.MapTest());
+		runner.add(new reactive.BindingsTest());
 		runner.add(new markup.FastTest());
 		runner.add(new markup.NodesTest());
-		runner.add(new collections.MapTest());
 		runner.add(new tween.TweenTest());
 		runner.run();
 	}
