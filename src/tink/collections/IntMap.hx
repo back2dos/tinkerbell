7eb3ef991f1d2b1334f59dd70b31828bc5c87fce
back2dos <back2dos@googlemail.com>
2012-03-08 12:02:33 +0100

    Made IntMap available for macro time.

--- src/tink/collections/IntMap.hx
+++ src/tink/collections/IntMap.hx
@@ -6,14 +6,21 @@ import tink.lang.Cls;
  * @author back2dos
  */
 
-class IntMap<T> implements Cls, implements Map < Int, T > {
-	@:forward var h:IntHash<T>;
-	public function new() {
-		this.h = new IntHash();
+#if macro
+	class IntMap<T> extends tink.collections.abstract.IntIDMap<Int, T> {
+		override function transform(k:Int) {
+			return k;
+		}
 	}
-	public inline function set(key:Int, value:T):T {
-		h.set(key, value);
-		return value;
+#else
+	class IntMap<T> implements Cls, implements Map < Int, T > {
+		@:forward var h:IntHash<T>;
+		public function new() {
+			this.h = new IntHash();
+		}
+		public inline function set(key:Int, value:T):T {
+			h.set(key, value);
+			return value;
+		}	
 	}
-	
-}
+#end
