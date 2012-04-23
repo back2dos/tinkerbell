67334371711cc2df295526045d1b57e5d0babfca
back2dos <back2dos@googlemail.com>
2012-04-23 14:02:09 +0200

    Just a couple of thoughts.

--- src/tink/reactive/Source.hx
+++ src/tink/reactive/Source.hx
@@ -13,3 +13,16 @@ interface Source<T> {
 interface Editable<T> implements Source<T> {
 	var value(get_value, set_value):T;
 }
+
+/*
+TODO: consider using this instead, after some profiling 
+typedef Source<T> = {
+	var value(get_value, null):T;
+	function unwatch(handler:Void->Void):Void;
+	function watch(handler:Void->Void):Void;
+}
+typedef Editable<T> = {
+	var value(get_value, set_value):T;
+	function unwatch(handler:Void->Void):Void;
+	function watch(handler:Void->Void):Void;
+}*/
