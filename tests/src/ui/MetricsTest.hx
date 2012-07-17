bd37633c468fcbed09f65faed467fcff81334862
back2dos <back2dos@googlemail.com>
2012-07-17 15:12:01 +0200

    Adapted test to new way of handling metrics.

--- tests/src/ui/MetricsTest.hx
+++ tests/src/ui/MetricsTest.hx
@@ -21,8 +21,11 @@ class MetricsTest extends TestCase {
 			new MockMetrics(symPair(100.0), symPair(.0), symPair(1.0)),
 			new MockMetrics(symPair(150.0), symPair(1.0), symPair(.5)),
 		];
-		assertEquals(150.0, group.minShort(true));
-		assertEquals(300.0, group.minLong(true, 0));
+		var metrics = [];
+		for (m in group) 
+			metrics.push(m.metrics);
+		assertEquals(150.0, metrics.minShort(true));
+		assertEquals(300.0, metrics.minLong(true, 0));
 	}
 	function testArrange() {
 		var group = [
@@ -30,34 +33,39 @@ class MetricsTest extends TestCase {
 			new MockMetrics(symPair(100.0), symPair(.0), symPair(1.0)),
 			new MockMetrics(symPair(150.0), symPair(1.0), symPair(.5)),
 		];
+		var metrics = [];
+		for (m in group) 
+			metrics.push(m.metrics);
 		
-		group.arrangeShort(true, 0, 600);
+		metrics.arrangeShort(true, 0, 600);
 		
 		assertEquals(600.0, group[0].dim.get(true));
 		assertEquals(200.0, group[1].dim.get(true));
 		assertEquals(150.0, group[2].dim.get(true));
 		
-		var min = group.minLong(true, 0);
+		var min = metrics.minLong(true, 0);
 		
-		group.arrangeLong(true, 0, min, 0);
+		metrics.arrangeLong(true, 0, min, 0);
 		for (m in group)
-			assertEquals(m.dim.get(true), m.getMin(true));
+			assertEquals(m.dim.get(true), m.metrics.getMin(true));
 		
 		for (i in 0...10) {
 			var total:Float = Math.round(min * (1 + 2 * Math.random()));
-			group.arrangeLong(true, 0, total, 0);
+			metrics.arrangeLong(true, 0, total, 0);
 			for (m in group)
 				total -= m.dim.get(true);
 			assertTrue(Math.abs(total) < 1e-10);
 		}
 	}
 }
-class MockMetrics extends Metrics {
+class MockMetrics {
 	public var dim:PlainPair<Float>;
 	public var pos:PlainPair<Float>;
+	public var metrics(default, null):Metrics;
 	public function new(min, align, weight) {
 		dim = new PlainPair(.0, .0);
 		pos = new PlainPair(.0, .0);
-		super(min, align, weight, pos.set, dim.set);
+		this.metrics = new Metrics(min, align, weight, pos.set, dim.set);
+		//super(min, align, weight, pos.set, dim.set);
 	}
 }
