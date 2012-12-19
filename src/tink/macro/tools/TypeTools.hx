75ad69270d6d71aaff3fedda71dcc1335418f1b9
back2dos <back2dos@googlemail.com>
2012-12-19 12:58:27 +0100

    Fixes in getFields. Cleanup.

--- src/tink/macro/tools/TypeTools.hx
+++ src/tink/macro/tools/TypeTools.hx
@@ -1,12 +1,11 @@
 package tink.macro.tools;
 
-/**
- * ...
- * @author back2dos
- */
+private typedef Enums = Type;
+
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.Type;
+import tink.core.types.Outcome;
 
 using tink.macro.tools.ExprTools;
 using tink.macro.tools.PosTools;
@@ -35,9 +34,9 @@ class TypeTools {
 				case AccNormal, AccInline: 'default';
 				case AccNo: 'null';
 				case AccNever: 'never';
-				case AccResolve: throw 'not implemented';
-				case AccRequire(r): 'not implemented';	
 				case AccCall(m): m;
+				default:
+					throw 'not implemented';
 			}		
 	}
 	static function getDeclaredFields(t:ClassType, out:Array<ClassField>, marker:Hash<Bool>) {
@@ -53,133 +52,91 @@ class TypeTools {
 			if (t.superClass != null)
 				getDeclaredFields(t.superClass.t.get(), out, marker);		
 	}
-	static function getBlankFields(t:ClassType, params:Array<Type>, out:Array<Field>, marker:Hash<Bool>) {
-		var subst = new Hash<ComplexType>();
-		for (i in 0...t.params.length) 
-			subst.set(
-				t.params[i].name,
-				switch (params[i]) {
-					case TInst(c, _):
-						var c = c.get();
-						if (c.kind == KTypeParameter) 
-							asComplexType(c.name);
-						else 
-							toComplex(params[i]);
-					default:
-						toComplex(params[i]);
-				}
-			);
-		
-		for (field in t.fields.get()) 
-			if (!marker.exists(field.name)) {
-				var kind = 
-					switch (field.kind) {
-						case FVar(read, write): 
-							FProp(
-								accessToName(read), 
-								accessToName(write), 
-								field.pos.makeBlankType()
-							);
-						case FMethod(k): 
-							switch (k) {
-								case MethMacro: null;
-								default: 
-									switch (reduce(field.type)) {
-										case TFun(args, _):
-											var argList = [];
-											for (arg in args)
-												argList.push(FunctionTools.toArg(arg.name, field.pos.makeBlankType()));
-											FFun({
-												args : argList,
-												ret: field.pos.makeBlankType(),
-												expr: null,
-												params: []
-											});
-										default: null;
-									}
-							}
-					}
-				if (kind != null) {
-					marker.set(field.name, true);
-					out.push( {
-						name: field.name,
-						pos: field.pos,
-						kind: kind,
-						meta: {
-							var res = field.meta.get();//this hopefully creates a copy
-							for (m in res)
-								for (i in 0...m.params.length)
-									m.params[i] = m.params[i].substParams(subst);
-							res;
-						},
-						access: field.isPublic ? [APublic] : [APrivate]
-					});					
-				}
-			}
-		if (t.isInterface)
-			for (t in t.interfaces)
-				getBlankFields(t.t.get(), t.params, out, marker);
-		else 
-			if (t.superClass != null)
-				getBlankFields(t.superClass.t.get(), t.superClass.params, out, marker);
-	}
-	static public function getFields(t:Type, ?substituteParams = true) {
+	
+	static var fieldsCache = new Hash();
+	static public function getFields(t:Type, ?substituteParams = true) 
 		return
 			switch (reduce(t)) {
-				case TInst(c, params): 
-					var c = c.get();
-					if (substituteParams) {//TODO: take a shortcut when the type has no parameters
-						var fields = [];
-						getBlankFields(c, params, fields, new Hash());
-						var anon = TAnonymous(fields);
-						var actual = toComplex(t);
-						return Context.currentPos().at(macro {
-							var actual : $actual = null;
-							var anon : $anon = actual;
-							anon;
-						}).typeof().map(function (t:Type)
-							return
-								switch (t) {
-									case TAnonymous(anon): anon.get().fields;
-									default: throw 'wtf just happened?';
-								}
-						);
-					}
-					else {
+				case TInst(c, _): 
+					var id = c.toString(),
+						c = c.get();
+					if (!fieldsCache.exists(id)) {
 						var fields = [];
 						getDeclaredFields(c, fields, new Hash());
-						fields.asSuccess();
+						fieldsCache.set(id, fields.asSuccess());
 					}
+					var ret = fieldsCache.get(id);
+					if (substituteParams && ret.isSuccess()) {
+						var e = ECheckType(macro null, toComplex(t)).at();
+						var fields = Reflect.copy(ret.sure());
+						ret = fields.asSuccess();
+						for (field in fields) 
+							if (field.isPublic) 
+								field.type = e.field(field.name, e.pos).typeof().sure();
+							else {
+								var kind = 
+									switch (field.kind) {
+										case FVar(read, write): 
+											FProp(accessToName(read), accessToName(write), field.pos.makeBlankType());
+										default: 
+											switch (reduce(field.type)) {
+												case TFun(args, _):
+													var argList = [];
+													for (arg in args)
+														argList.push(
+															FunctionTools.toArg(arg.name, field.pos.makeBlankType())
+														);
+													FFun({
+														args : argList,
+														ret: field.pos.makeBlankType(),
+														expr: null,
+														params: []
+													});
+												default: null;
+											}
+									}
+								if (kind != null) {
+									var f = {
+										name: field.name,
+										pos: field.pos,
+										kind: kind,
+										access: [APrivate]
+									};				
+									field.type = ECheckType(e, TAnonymous([f])).at(e.pos).field(field.name, e.pos).typeof().sure();
+								}								
+							}
+						}
+					ret;
 				case TAnonymous(anon): anon.get().fields.asSuccess();
 				default: Context.currentPos().makeFailure('type has no fields');
 			}
-	}
-	static public function getStatics(t:Type) {
+	
+	static public function getStatics(t:Type) 
 		return
 			switch (reduce(t)) {
 				case TInst(t, _): t.get().statics.get().asSuccess();
 				default: 'type has no statics'.asFailure();
 			}
-	}
-	static public function toString(t:ComplexType) {
-		return Printer.printType('', t);
-	}
-	static public function isSubTypeOf(t:Type, of:Type, ?pos) {
+	
+	static public function toString(t:ComplexType) 
+		return Printer.printType('', t)
+	
+	static public function isSubTypeOf(t:Type, of:Type, ?pos) 
 		return 
-			ECheckType(ECheckType('null'.resolve(), toComplex(t)).at(pos), toComplex(of)).at(pos).typeof();
-	}
-	static public function isDynamic(t:Type) {
+			ECheckType(ECheckType('null'.resolve(), toComplex(t)).at(pos), toComplex(of)).at(pos).typeof()
+	
+	static public function isDynamic(t:Type) 
 		return switch(reduce(t)) {
 			case TDynamic(_): true;
 			default: false;
 		}
-	}
-	static public function toType(t:ComplexType, ?pos) {	
+	
+	static public function toType(t:ComplexType, ?pos) 
 		return [
 			'_'.define(t, pos),
 			'_'.resolve(pos)
-		].toBlock(pos).typeof();
-	}
+		].toBlock(pos).typeof()
+	
 	static public inline function instantiate(t:TypePath, ?args, ?pos) {
 		return ENew(t, args == null ? [] : args).at(pos);
 	}
@@ -199,12 +156,12 @@ class TypeTools {
 			sub: sub
 		}
 	}
-	static public inline function asComplexType(s:String, ?params) {
-		return TPath(asTypePath(s, params));
-	}
-	static public inline function reduce(type:Type, ?once) {
-		return Context.follow(type, once);
-	}
+	static public inline function asComplexType(s:String, ?params) 
+		return TPath(asTypePath(s, params))
+	
+	static public inline function reduce(type:Type, ?once) 
+		return Context.follow(type, once)
+	
 	static public function isVar(field:ClassField) {
 		return switch (field.kind) {
 			case FVar(_, _): true;
@@ -222,12 +179,12 @@ class TypeTools {
 			ret.push(TPType(toComplex(p, true)));
 		return ret;
 	}
-	static function baseToComplex(t:BaseType, params:Array<Type>) {
-		return asComplexType(t.module + '.' + t.name, paramsToComplex(params));
-	}
+	static function baseToComplex(t:BaseType, params:Array<Type>) 
+		return asComplexType(t.module + '.' + t.name, paramsToComplex(params))
+	
 	static public function toComplex(type:Type, ?pretty = false):ComplexType {
 		return 
-			if (pretty) {
+			if (pretty) 
 				switch (type) {
 					case TEnum(t, params):
 						var t = t.get();
@@ -257,7 +214,6 @@ class TypeTools {
 					//TODO: check TDynamic here
 					default: toComplex(type, false);
 				}
-			}
 			else
 				TPath({
 					pack : ['haxe','macro'],
