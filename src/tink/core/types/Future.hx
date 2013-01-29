696b6de01961621dec956979f2d427b3ef9d81f3
back2dos <back2dos@googlemail.com>
2013-01-29 13:22:13 +0000

    Defined Suprise as future outcome. Moved out extension methods from Future to Surprise accordingly.

--- src/tink/core/types/Future.hx
+++ src/tink/core/types/Future.hx
@@ -28,39 +28,3 @@ class FutureTools {
 					handler(map(result))
 				)
 }
-
-class FutureOutcomeTools {
-	static public function map<A, B, F>(f:Future<Outcome<A, F>>, map:A->B):Future<Outcome<B, F>>
-		return 
-			FutureTools.map(f, function (outcome) 
-				return
-					switch (outcome) {
-						case Success(s): Success(map(s));
-						case Failure(f): Failure(f);
-					}
-			)
-			
-	static public function chain<A, B, F>(f:Future<Outcome<A, F>>, next:A->Future<Outcome<B, F>>):Future<Outcome<B, F>>
-		return
-			FutureTools.chain(f, function (result) 
-				return
-					switch (result) {
-						case Success(result): next(result);
-						case Failure(error): function (handler) handler(Failure(error));
-					}
-			)
-			
-	static public function handle < S, F > (f:Future < Outcome < S, F >> , ?process:S->Void, ?recover:F->Void, ?finally:Void->Void) {
-		f(function (outcome) {
-			switch (outcome) {
-				case Success(s):
-					if (process != null) process(s);
-				case Failure(f):
-					if (recover != null) recover(f);
-			}
-			if (finally != null) finally();
-		});
-		return f;
-	}
-	
-}
---
+++ src/tink/core/types/Surprise.hx
@@ -0,0 +1,42 @@
+package tink.core.types;
+
+import tink.core.types.Future;
+import tink.core.types.Outcome;
+
+typedef Surprise<S, F> = Future<Outcome<S, F>>;
+
+class SurpriseTools {
+	static public function map<A, B, F>(f:Surprise<A, F>, map:A->B):Surprise<B, F>
+		return 
+			FutureTools.map(f, function (outcome) 
+				return
+					switch (outcome) {
+						case Success(s): Success(map(s));
+						case Failure(f): Failure(f);
+					}
+			)
+			
+	static public function chain<A, B, F>(f:Surprise<A, F>, next:A->Surprise<B, F>):Surprise<B, F>
+		return
+			FutureTools.chain(f, function (result) 
+				return
+					switch (result) {
+						case Success(result): next(result);
+						case Failure(error): function (handler) handler(Failure(error));
+					}
+			)
+			
+	static public function handle<S, F>(f:Surprise<S, F>, ?process:S->Void, ?recover:F->Void, ?finally:Void->Void) {
+		f(function (outcome) {
+			switch (outcome) {
+				case Success(s):
+					if (process != null) process(s);
+				case Failure(f):
+					if (recover != null) recover(f);
+			}
+			if (finally != null) finally();
+		});
+		return f;
+	}
+	
+}
