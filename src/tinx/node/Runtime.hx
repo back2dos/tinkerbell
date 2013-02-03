e4b43021208707b7168fac4a710ce775d559e664
back2dos <back2dos@googlemail.com>
2013-02-03 23:12:00 +0000

    Renamed to avoid collision.

--- src/tinx/node/Runtime.hx
+++ src/tinx/node/Runtime.hx
@@ -1,11 +1,15 @@
 package tinx.node;
 
-@:native('process') extern class Lib {
+@:native('process') extern class Runtime {
 	static public function nextTick(f:Void->Void):Void;
 	static public var platform(default, null):String;
 	static public function binding<A>(id:String):A;
 	static public function cwd():String;
-	static public inline function module<A>(s:String):A 
+	
+	static public var argv(default, null):Array<String>;
+	static public var exexPath(default, null):String;
+	
+	static public inline function load<A>(s:String):A
 		return untyped require(s)
 	static public inline function env(id:String):String 
 		return untyped process[id]
