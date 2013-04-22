627c127f89b18228f89e28e358449763a9858bc8
back2dos <back2dos@googlemail.com>
2013-04-22 09:46:29 +0200

    Callback is an abstract now.

--- src/tink/core/types/Callback.hx
+++ src/tink/core/types/Callback.hx
@@ -1,22 +1,62 @@
 package tink.core.types;
 
-typedef Callback<T> = T->Void;
-
-abstract CallbackLink(Void->Bool) {
-	public inline function new(link:Void->Bool) this = link;
-	public function cancel():Bool return asFunction(this)();
-	inline function asFunction():Void->Bool return this;	
-	@:from static inline function fromFunction(f:Void->Bool) return new CallbackLink(f);
+abstract Callback<T>(T->Void) from (T->Void) {
+	inline function new(f) 
+		this = f;
+	public inline function invoke(data:T):Void 
+		(this)(data);
+	@:from static inline function fromNiladic<A>(f:Void->Void):Callback<A> 
+		return new Callback(function (r) f());
 }
 
-//TODO: There's a lot of room for optimization here
-abstract CallbackList<T>(Array<Callback<T>>) {
-	public inline function new() this = [];
-	public function add(cb:Callback<T>):CallbackLink {
-		var f = function (v) cb(v);
-		this.push(f);
-		return new CallbackLink(function () return this.remove(f));
+abstract CallbackLink(Void->Void) {
+	inline function new(link:Void->Void) 
+		this = link;
+	public function dissolve():Void 
+		if (this != null) asFunction(this)();
+	inline function asFunction():Void->Void 
+		return this;	
+	@:from static inline function fromFunction(f:Void->Void) 
+		return new CallbackLink(f);
+}
+
+private class Cell<T> {
+	//TODO: the cell (or some super class of it) could just as easily act as callback link
+	public var cb:Callback<T>;
+	function new() {}
+	public inline function free():Void {
+		this.cb = null;
+		pool.push(this);
 	}
+	static var pool:Array<Cell<Dynamic>> = [];
+	static public inline function get<A>():Cell<A> 
+		return
+			if (pool.length > 0)cast pool.pop();
+			else new Cell();
+}
+
+abstract CallbackList<T>(Array<Cell<T>>) {
+	public var length(get, never):Int;
+	public inline function new() 
+		this = [];
+	inline function get_length():Int 
+		return this.length;	
+	public function add(cb:Callback<T>):CallbackLink {
+		var cell = Cell.get();
+		cell.cb = cb;
+		this.push(cell);
+		return function () {
+			this.remove(cell);
+			cell.free();
+		}
+	}
+	@:to public function toSignal():Signal<T> 
+		return new Signal(add.bind(this));
+		
 	public function invoke(data:T) 
-		for (f in this.copy()) f(data);
+		for (cell in this.copy()) 
+			cell.cb.invoke(data);			
+	public function clear():Void 
+		for (cell in this.splice(0, this.length)) 
+			cell.free();
 }
