b229845577aa2be495696f227396a054d6dc7a1f
back2dos <back2dos@gmail.com>
2013-07-05 18:01:35 +0200

    Make ObjectID an abstract for easier handling.

--- src/tinx/node/mongo/ObjectID.hx
+++ src/tinx/node/mongo/ObjectID.hx
@@ -1,9 +1,25 @@
 package tinx.node.mongo;
 
+import tink.core.types.Outcome;
+import tinx.node.Error;
+import tinx.node.Runtime;
+
 @:native('require("mongodb").ObjectID')
-extern class ObjectID {
-	public function new(?s:String):Void;
-	public function toString():String;
-	public function valueOf():String;
-	static public inline function gen():String return new ObjectID().valueOf();		
+private extern class Native {
+	function new(?s:String);
+	function toString():String;
+}
+abstract ObjectID(Native) {
+	public function new(?s:String) this = new Native(s); 
+	@:to public function toString():String return this.toString();
+	static public function parse(s:String):Outcome < ObjectID, Error > {
+		try {
+			return Success(new ObjectID(s));
+		}
+		catch (e:Dynamic) 
+			return Failure(e);
+	}
+	@:to public function toDate():Date {
+		return Date.fromTime((untyped parseInt)(this.toString().substr(0,8), 16)*1000);
+	}
 }
