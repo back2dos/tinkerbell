25cdd2ffdb7401c4f21d12426186b36032afd591
back2dos <back2dos@gmail.com>
2013-07-05 18:06:55 +0200

    Add setEncoding from native API.

--- src/tinx/node/io/NativeIn.hx
+++ src/tinx/node/io/NativeIn.hx
@@ -4,6 +4,7 @@ import tinx.node.events.Emitter;
 
 typedef NativeIn = {>Emitter,
 	var readable(default, null):Bool;
+	function setEncoding(encoding:String):Void;
 	function pause():Void;
 	function resume():Void;
 	function destroy():Void;
